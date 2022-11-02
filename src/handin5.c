1b471fabcab59f5fe28a1fcccb423c3938f54612
sebub <105357150+sebub@users.noreply.github.com>
2022-11-02 19:57:48 +0100

    Update handin5.c

--- src/handin5.c
+++ src/handin5.c
@@ -3,22 +3,38 @@
 #include <math.h>
 
 int is_palindrome_iter(char *str);
-int main(void)
-{
-    char *str = "gtootg";
-    int test = is_palindrome_iter(str);
-    printf("%d",test);
 
+int is_palindrome_rec_test(char *str, int length);
+
+int is_palindrome_rec(char *str);
+
+int main(void) {
+    char* str = "llgoogll";
+
+    printf("%d\n", is_palindrome_iter(str));
+
+    printf("%d\n", is_palindrome_rec(str));
 }
 
-int is_palindrome_iter(char *str){
-    int len = strlen(str)/ sizeof(char);
-    int runs = floor(len/2);
+int is_palindrome_iter(char *str) {
+    int len = strlen(str) / sizeof(char);
+    int runs = floor(len / 2);
     for (int i = 0; i < runs; ++i) {
-        printf("%c",str[i]);
-        printf("%c",str[len-(i+1)]);
-        if(str[i]!=str[len-(i+1)])
+        if (str[i] != str[len - (i + 1)])
             return 0;
     }
     return 1;
 }
+
+int is_palindrome_rec(char *str) {
+    return is_palindrome_rec_test(str, strlen(str));
+}
+
+int is_palindrome_rec_test(char *str, int length) {
+    if(length<2)
+        return 1;
+    else
+        return str[0] == str[length - 1] && is_palindrome_rec_test(str + 1, length - 2);
+
+}
+
